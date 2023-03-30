3503fa0c92c52867fa1bde9461fb1aea4f75c2ac
mahesh <gangurdemahesh9@gmail.com>
2023-03-30 21:18:21 +0530

    [mahesh]add program file for que 1 e.g.‘a’ followed by two to three 'b'

---
+++ Day8_Regular_Expressions/Pattern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day8_Regular_Expressions
+{
+    public class Pattern
+    {
+        public static void Question1(string inputString)
+        {
+            Regex regex = new Regex(@"a[b]{2,3}");
+
+            if (regex.IsMatch(inputString))
+            {
+                Console.WriteLine("The input string '{0}' matches the pattern.", inputString);
+            }
+            else
+            {
+                Console.WriteLine("The input string '{0}' does not match the pattern.", inputString);
+            }
+        }
+
+    }
+
+}
+
+
--- Day8_Regular_Expressions/Program.cs
+++ Day8_Regular_Expressions/Program.cs
@@ -11,6 +11,18 @@ namespace Day8_Regular_Expressions
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Day 8 Practice Problem for Regular Expressions");
+            Console.WriteLine("Select one Option for \n 1. Write a C# program that matches a string that has an ‘a’ followed by two to three 'b'");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Pattern.Question1("mahesh ab Gangurde");
+                    break;
+
+                default:
+                    Console.WriteLine("Please Select Correct option");
+                    break;
+            }
         }
     }
 }
