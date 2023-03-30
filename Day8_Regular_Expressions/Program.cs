6f50e304cf10b2bbb0d7257f445fc62c9e6efdb7
mahesh <gangurdemahesh9@gmail.com>
2023-03-30 20:48:55 +0530

    [mahesh]add program file for welcome greeting

---
+++ Day8_Regular_Expressions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_Regular_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Day 8 Practice Problem for Regular Expressions");
+        }
+    }
+}
